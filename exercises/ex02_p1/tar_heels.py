279424db63ec76fc928de1b938182b9c41cdd770
Zachary Martens <your@primary.email>
2021-02-09 20:45:21 -0500

    Finished EX02_PT01

--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -1,15 +1,29 @@
 """Tar Heels exercise redux as a structured program."""
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730317621"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
-    # TODO 2: Print the response of calling the tar_heels function here.
+    print(tar_heels(choice))
 
 
-# TODO 1: Define the tar_heels function, and its logic, here.
+def tar_heels(tarithmetic: int) -> str:
+    """Function that checks divisibility of various numbers by 2 and 7."""
+
+    output = ""
+
+    if tarithmetic % 2 == 0:
+        output = output + "TAR"
+    if tarithmetic % 14 == 0: 
+        output = output + " "
+    if tarithmetic % 7 == 0:
+        output = output + "HEELS"
+    if tarithmetic % 7 == 0 or tarithmetic % 2 == 0: 
+        return output
+    else:
+        return "CAROLINA"
 
 
 if __name__ == "__main__":
