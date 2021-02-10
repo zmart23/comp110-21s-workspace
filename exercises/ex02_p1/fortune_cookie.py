b34adcd90e8365eb9a299bb973f494bc99867564
Zachary Martens <your@primary.email>
2021-02-09 20:17:38 -0500

    Smack

--- exercises/ex02_p1/fortune_cookie.py
+++ exercises/ex02_p1/fortune_cookie.py
@@ -2,17 +2,28 @@
 
 from random import randint
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730317621"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Your fortune cookie says...")
-    # TODO 2: Print the result of calling your fortune_cookie function.
+    print(fortune_cookie())
     print("Now, go spread positive vibes!")
 
+from random import randint
 
-# TODO 1: Define your fortune_cookie function here.
+def fortune_cookie(a: int = int(randint(1, 100))) -> str:
+    """Definition of the fortune_cookie function."""
+    if a < 60:
+        if a < 30:
+            return "You will be gifted with a large sum of money."
+        return "You will live a long and happy life."
+    else:
+        if a < 90:
+            return "Everything you do today will prosper."
+        return "You will find love in the near future."
+
 
 
 # Python Idiom for "starting" the program when run as a module.
