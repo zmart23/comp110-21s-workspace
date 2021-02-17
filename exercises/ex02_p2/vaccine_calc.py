1e381b3ef1c9c55dea571497e45b8645173ef214
Zachary Martens <your@primary.email>
2021-02-17 14:57:34 -0500

    Finished EX02.P2

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -14,7 +14,7 @@ def main() -> None:
     daytota = days_to_target(population, doses, doses_per_day, target)
     fudat = future_date(daytota)
     leng: int = int(daytota)
-    dat: datetime = str(fudat)
+    dat = str(fudat)
     print("We will reach " + str(target) + "% vaccination in " + str(leng) + " days, which falls on " + str(dat) + ".")
 
 
@@ -29,7 +29,7 @@ def future_date(howmanydays: int) -> str:
     today: datetime = datetime.today()
     target_vaccination_length: timedelta = timedelta(howmanydays)
     future: datetime = today + target_vaccination_length
-    time: datetime = future.strftime("%B %d, %Y")
+    time = future.strftime("%B %d, %Y")
     fut_time = str(time)
     return fut_time
 
