5535ea6c6e31c166b14c2e3c95c9383678198c28
Zachary Martens <your@primary.email>
2021-02-26 17:58:12 -0500

    kj

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -7,35 +7,35 @@ from random import randint
 
 def main() -> None:
     """The entrypoint of the module, when run as a program."""
+    points = 0
     greet()
-    points: int = int(adventure_points)
-    print("Menu: ")
-    print ("Which Star Wars Vehicle Are You?")
-    print ("Which Star Wars Character Are You?")
-    print ("Finish Experience")
-    print ()
-    response: str = str(input("Which game would you like to play?"))
+    print()
+    print("Choose A Game: ")
+    print("Which Star Wars Vehicle Are You?")
+    print("Which Star Wars Character Are You?")
+    print("Which Star Wars Planet Do You Live On?")
+    print("Finish Experience")
+    print()
+    response: str = str(input("Which game would you like to play? "))
     a: str = str("Which Star Wars Vehicle Are You?")
     b: str = str("Which Star Wars Character Are You?")
-    while True:
-        if response == a:
-            sw_vehicle_quiz (points: int)
-        if response == b:
-            sw_character_quiz (points)
-        else:
-            print(f"You have {points} adventure points.")
-    
-    
+    c: str = str("Finish Experience.")
+    while response == a:
+        sw_vehicle_quiz(points)
+    print()
+        
+
 def greet() -> None:
     """Greeting message to player."""
-    player: str = input("What is your name?")
+    player: str = input("What is your name? ")
+    print()
     print(f"Hello, {player}, welcome to Star Wars Mania. In this game, there will be two quiz options available which will ask you various questions and determine something in Star Wars that relates to you. For each game played, you will receive a fixed amount of adventure points. Different prizes will be rewarded for different point totals.")
 
 
 def sw_vehicle_quiz(points: int) -> int:
     """Quiz that takes various inputs to decide what Star Wars vehicle you pilot."""
+    global points
     main()
-    greet()
     print(f"Well, {player}, this game is called 'What Star Wars Vehicle Do You Pilot?.' You will be asked a series of questions that may seem unrelated but these questions will indeed help me to determine what Star Wars vehicle you would pilot. For each time you complete the quiz, you will earn 50 adventure points.")
     material: str = str(input("Would you like a ship made of wood or metal?"))
     seats: str = int(input("Would you like 2 seats or 4 seats?"))
@@ -44,50 +44,77 @@ def sw_vehicle_quiz(points: int) -> int:
     metal: str = str
     fast: str = str
     slow: str = str
-    if material = wood:
-        if seats = 2:
-            if speed = fast:
+    if material == wood:
+        if seats == 2:
+            if speed == fast:
                 print("You pilot a speeder bike.")
-    if material = wood:
-        if seats = 4:
-            if speed = fast:
+    if material == wood:
+        if seats == 4:
+            if speed == fast:
                 print("You pilot a star destroyer.")
-    if material = wood:
-        if seats = 2:
-            if speed = slow:
+    if material == wood:
+        if seats == 2:
+            if speed == slow:
                 print("You pilot a medical frigate.")
-    if material = wood:
-        if seats = 4:
-            if speed = slow:
+    if material == wood:
+        if seats == 4:
+            if speed == slow:
                 print("You pilot the Death Star.")
-    if material = metal:
-        if seats = 2:
-            if speed = fast:
+    if material == metal:
+        if seats == 2:
+            if speed == fast:
                 print("You pilot a snowspeeder.")
-    if material = metal:
-        if seats = 4:
-            if speed = fast:
+    if material == metal:
+        if seats == 4:
+            if speed == fast:
                 print("You pilot the Millennium Falcon.")
-    if material = metal:
-        if seats = 2:
-            if speed = slow:
+    if material == metal:
+        if seats == 2:
+            if speed == slow:
                 print("You pilot an X-wing Starfighter")
-    if material = metal:
-        if seats = 4:
-            if speed = slow:
+    if material == metal:
+        if seats == 4:
+            if speed == slow:
                 print("You pilot a sandcrawler.")
     else:
         print ()
+   
 
-
-def sw_character_quiz (points):
-    """Quiz that takes various characteristic inputs to decide which Star Wars character you are."""
+def sw_planet_quiz(points: int -> int:)
+    "Quiz that takes various inputs to decide which Star Wars planet you live on."
     global points
-    points = 
+    main()
+    print(f"Well, {player}, this game is called 'Which Star Wars Planet Do You Live On?.' You will be asked a series of questions that may seem unrelated but these questions will indeed help me to determine what Star Wars planet you live on. For each time you complete the quiz, you will earn 50 adventure points.")
+    terrain: str = str(input("What kind of terrain does your planet have: desert or forest?"))
+    weather: str = str(input("What kind of weather is frequent on your planet: rainy or sunny?"))
+    desert: str = str
+    forest: str = str
+    rainy: str = str
+    sunny: str = str
+    if terrain == desert:
+        if weather == rainy:
+            print("You live on Naboo.")
+    if terrain == desert:
+        if weather == sunny:
+            print("You live on Hoth.")
+    if terrain == forest:
+        if weather == rainy:
+            print("You live on Endor.")
+    if terrain == forest:
+        if weather == sunny:
+            print("You live on Coruscant.")
+
+def sw_character quiz() -> None:
+    "Quiz that takes various inputs to decide which Star Wars character you are."
+    global points
+    print(f"Well, {player}, this game is called 'Which Star Wars Character Are You?.'You will be asked a series of questions that may seem unrelated but these questions will indeed help me to determine what Star Wars character you are. For each time you complete the quiz, you will earn 50 adventure points.")
+
 
 
 def end_message () -> None:
     "Prints goodbye message with total accumulated adventure points."
+    global points
+    print(f"Goodbye, you have {points} adventure points.")
 
 
 if __name__ == "__main__":
