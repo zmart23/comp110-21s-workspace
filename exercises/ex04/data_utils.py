143eb3b8da72051e757023a4b547322eed6f2a97
Zachary Martens <your@primary.email>
2021-03-26 15:21:37 -0400

    Progress on EX 04

--- exercises/ex04/data_utils.py
+++ exercises/ex04/data_utils.py
@@ -1,18 +1,52 @@
 """Utility functions for wrangling data."""
 
-__author__ = "YOUR PID HERE"
+__author__ = "730317621"
 
 
-from csv import DictReader
+from  csv import DictReader
 
 
 def read_csv_rows(csv_file: str) -> list[dict[str, str]]:
     """Read a CSV file's contents into a list of rows."""
     rows: list[dict[str, str]] = []
-    
-    # TODO 0.1) Complete the implementation of this function here.
-
+    file_handle = open(csv_file, "r")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        rows.append(row)
+    file_handle.close()
     return rows
 
 
-# TODO: Define the other functions here.
+def column_values(row: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list of all values in a single column whose name is the second parameter."""
+    column_val: list[str] = []
+    for line in row:
+        column_val.append(line[column])
+    return column_val
+
+
+def columnar(rowed: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a table represented as a list of rows into one represented as a dictionary of columns."""
+    row_column: dict[str, list[str]] = {}
+    for row in rowed[0]:
+        row_column[row] = column_values(rowed, row)
+    return row_column
+
+
+def head(rowing: dict[str, list[str]], columning: int) -> dict[str, list[str]]:
+    rowing_columning: dict[str, list[str]] = {}
+    for column in rowing:
+        first_values: list[int] = []
+        i = 0
+        for i in range(columning):
+            first_values.append(rowing[column][i])
+        rowing_columning[column] = first_values
+    return rowing_columning
+
+
+def select(ro: dict[str, list[str]], col: list[str]) -> dict[str, list[str]]:
+    ro_col: dict[str, list[str]] = {}
+
+
+def count(val_freq: list[str]) -> dict[str, int]
+    freq: dict[str, int] = {}
